30a90093fbb53e626a5528b02b034e6c409f3534
Patricia Vera Hernández <p.vhernandez@hotmail.com>
2021-09-09 13:04:43 +0200

    Menu

--- src/Gallery.java
+++ src/Gallery.java
@@ -8,23 +8,87 @@ public class Gallery {
     private final JPanel mainPanel;
     private final JLabel statusBar;
 
+    private final JMenuBar menuBar;
+
+    private final JMenu mainMenu;
+    private final JMenu fileMenu;
+    private final JMenu viewMenu;
+
+    private final JMenuItem iDelete, iImport, iQuit;
+    private final JCheckBoxMenuItem iViewer, iBrowser;
+
     public Gallery() {
         mainFrame = new JFrame("Welcome to your PhotoBrowser");
         mainFrame.setVisible(true);
 
+        menuBar = new JMenuBar();
+
+        mainMenu = new JMenu("Menu");
+        fileMenu = new JMenu("File");
+        viewMenu = new JMenu("View");
+
         mainPanel = new JPanel();
         statusBar = new JLabel();
 
+        iImport = new JMenuItem("Import");
+        iDelete = new JMenuItem("Delete");
+        iQuit = new JMenuItem("Quit");
+        iViewer = new JCheckBoxMenuItem("Viewer", true);
+        iBrowser = new JCheckBoxMenuItem("Browser", false);
+
         setUpUI();
 
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.pack();
     }
 
     private void setUpUI() {
         mainFrame.setPreferredSize(new Dimension(600, 500));
+        // mainFrame.setMinimumSize(new Dimension(100, 100));
 
+        setUpAppMenu();
         setUpMainSpace();
         setUpStatusBar();
+    }
+
+    private void setUpAppMenu() {
+        fileMenu.add(iImport);
+        fileMenu.add(iDelete);
+        fileMenu.add(iQuit);
+        fileMenu.setBorder(new EmptyBorder(5, 5, 5, 5));
+        //fileMenu.setMinimumSize(new Dimension(150, 0));
+
+        viewMenu.add(iViewer);
+        viewMenu.add(iBrowser);
+        viewMenu.setBorder(new EmptyBorder(5, 5, 5, 5));
+        //viewMenu.setMinimumSize(new Dimension(150, 0));
+        iViewer.setState(true);
+        iBrowser.setState(false);
+        setUpCheckMenuItemsListeners();
+
+        mainMenu.add(fileMenu);
+        mainMenu.add(viewMenu);
+
+        menuBar.add(mainMenu);
+        mainFrame.setJMenuBar(menuBar);
+    }
+
+    private void setUpCheckMenuItemsListeners() {
+        // In this function will be implemented the way
+        // the app changes from one mode to another
+        iViewer.addActionListener(listener -> {
+            if (!iViewer.getState()) {
+                iBrowser.setState(false);
+                iViewer.setState(true);
+            }
+        });
+
+        iBrowser.addActionListener(listener -> {
+            if (!iBrowser.getState()) {
+                iViewer.setState(false);
+                iBrowser.setState(true);
+            }
+        });
     }
 
     private void setUpMainSpace() {
