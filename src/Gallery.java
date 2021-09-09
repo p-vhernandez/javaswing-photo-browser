2906edc29ce0a2f91bd526da9c0329b8445892d2
Patricia Vera Hernández <p.vhernandez@hotmail.com>
2021-09-09 11:36:55 +0200

    Status bar

--- src/Gallery.java
+++ src/Gallery.java
@@ -1,13 +1,17 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class Gallery {
 
     private final JFrame mainFrame;
+    private final JLabel statusBar;
 
     public Gallery() {
-        mainFrame = new JFrame("Welcome to your PhotoBrowser!");
+        mainFrame = new JFrame("Welcome to your PhotoBrowser");
         mainFrame.setVisible(true);
+
+        statusBar = new JLabel();
 
         setUpUI();
 
@@ -16,6 +20,14 @@ public class Gallery {
 
     private void setUpUI() {
         mainFrame.setPreferredSize(new Dimension(600, 500));
+
+        setUpStatusBar();
+    }
+
+    private void setUpStatusBar() {
+        statusBar.setText("This is a dummy text as a status");
+        statusBar.setBorder(new EmptyBorder(5, 5, 5, 5));
+        mainFrame.add(statusBar, BorderLayout.SOUTH);
     }
 
 }
