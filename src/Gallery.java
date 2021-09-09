253345ba9e932e51e237ed99939867f30f3b5062
Patricia Vera Hernández <p.vhernandez@hotmail.com>
2021-09-09 11:45:04 +0200

    main empty space

--- src/Gallery.java
+++ src/Gallery.java
@@ -5,12 +5,14 @@ import java.awt.*;
 public class Gallery {
 
     private final JFrame mainFrame;
+    private final JPanel mainPanel;
     private final JLabel statusBar;
 
     public Gallery() {
         mainFrame = new JFrame("Welcome to your PhotoBrowser");
         mainFrame.setVisible(true);
 
+        mainPanel = new JPanel();
         statusBar = new JLabel();
 
         setUpUI();
@@ -21,7 +23,15 @@ public class Gallery {
     private void setUpUI() {
         mainFrame.setPreferredSize(new Dimension(600, 500));
 
+        setUpMainSpace();
         setUpStatusBar();
+    }
+
+    private void setUpMainSpace() {
+        // Background just to check if it's visible
+        // Keep it empty as assignment states
+        mainPanel.setBackground(new Color(168, 237, 206));
+        mainFrame.add(mainPanel, BorderLayout.CENTER);
     }
 
     private void setUpStatusBar() {
